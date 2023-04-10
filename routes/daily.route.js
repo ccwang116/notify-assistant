09c9834e0485d19da99706002e3c2b69f49e5b11
Ivy Wang <66878095+ccwang116@users.noreply.github.com>
2023-04-10 16:18:04 +0800

    Add res status

--- routes/daily.route.js
+++ routes/daily.route.js
@@ -7,11 +7,15 @@ const { getStockInfo } = require("../controllers/tracks.controller");
 router.get("/", (req, res, next) => {
   res.send("OK");
 });
-router.get("/weather", () => {
+router.get("/weather", (req, res, next) => {
   const list = mockData.weather;
   getWeatherInfo(list[0].locationId, list[0].locationName);
+  res.status(200).json({
+    message: "Send Success!",
+    data: list,
+  });
 });
-router.get("/stock", () => {
+router.get("/stock", (req, res, next) => {
   console.log("Wait for 5 second...");
   const list = mockData.tracks;
   console.log(list);
@@ -19,6 +23,10 @@ router.get("/stock", () => {
     const element = list[i];
     getStockInfo(element.stockId, element.tracePrice);
   }
+  res.status(200).json({
+    message: "Fetch Success!",
+    data: list,
+  });
 });
 
 module.exports = router;
