c6a0773fce9061d67a79f6d11b93d7df5081f1ad
Ivy Wang <66878095+ccwang116@users.noreply.github.com>
2023-04-10 16:10:32 +0800

    apply basic page by mock data

--- routes/under-construction.route.js
+++ routes/under-construction.route.js
@@ -1,8 +1,42 @@
 const express = require("express");
 const router = express.Router();
+const mockData = require("../db.json");
 
 router.get("/", (req, res) => {
-  res.render("underConstruction");
+  const baseUrl = req.baseUrl;
+  let list = {};
+  switch (baseUrl) {
+    case "/weather":
+      list = mockData.weather;
+      res.render("weather", {
+        title: "Weather page",
+        locationId: list[0].locationId,
+        currentLocation: list[0].locationName,
+        form_action: "/weather/edit",
+      });
+      break;
+    case "/stock":
+      list = mockData.tracks;
+      res.render("stock", {
+        initialValue: { stockId: "", tracePrice: "" },
+        title: "Stock Tracker",
+        results: list,
+        form_action: "/tracks",
+      });
+      break;
+    case "/bus":
+      list = mockData.bus;
+      res.render("bus", {
+        title: "Bus page",
+        routeId: list[0].routeId,
+        stopId: list[0].stopId,
+        form_action: "/bus/edit",
+      });
+      break;
+
+    default:
+      break;
+  }
 });
 
 module.exports = router;
