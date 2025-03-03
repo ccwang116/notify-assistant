53148186f0ece550da352b46c5d2551f293be8a6
Ivy Wang <ivy.wang@clinico.com.tw>
2025-03-03 14:58:16 +0800

    update to LINE Messaging API

--- controllers/notify.controller.js
+++ controllers/notify.controller.js
@@ -1,21 +1,41 @@
 require("dotenv").config();
 const axios = require("axios");
 
-const webhook_url = "https://notify-api.line.me/api/notify";
+const webhook_url = "https://api.line.me/v2/bot/message/push";
 
 const oauthToken = process.env["LINE_NOTIFY_OAUTHTOKEN_ME"];
 
+const userId = "Uc4f6f41cfd8c215cf3df4d2f08d68847"
+
 const makeNotify = async (text, isSticker = false, stickerId = [0, 0]) => {
-  const data = new URLSearchParams();
-  data.append("message", text);
+  const data = {
+    to: userId,
+    messages: [
+      {
+        type: "text",
+        text: text,
+      },
+    ],
+    notificationDisabled: false,
+  };
+
   if (isSticker) {
-    data.append("stickerPackageId", stickerId[0]);
-    data.append("stickerId", stickerId[1]);
+    data.messages = [
+      {
+        type: "text",
+        text: text,
+      },
+      {
+        type: "sticker",
+        packageId: stickerId[0],
+        stickerId: stickerId[1],
+      },
+    ];
   }
   try {
     const result = await axios.post(webhook_url, data, {
       headers: {
-        "content-type": "application/x-www-form-urlencoded;charset=utf-8",
+        "content-type": "application/json",
         Authorization: "Bearer " + oauthToken,
       },
     });
